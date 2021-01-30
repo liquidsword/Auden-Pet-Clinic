7b89915ae8144d14ed36c796f10e1bcb9773057a
Auden Robertson <audenrobertson@gmail.com>
2021-01-29 22:57:35 -0500

    created list, search and add components

---
+++ interface/src/components/AddAppointments.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AddApointments extends Component {
+    render() {
+        return <div>Add Appointments</div>;
+    }
+}
+
+export default AddApointments;
