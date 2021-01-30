f7402d8f82b2bad0017a939d0a2482bddd70d48a
Auden Robertson <audenrobertson@gmail.com>
2021-01-29 21:51:24 -0500

    updated folders and app.js

--- interface/index.js
+++ interface/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'boostrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/index.css';
+import App from './src/components/App';
+import reportWebVitals from './src/reportWebVitals';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
